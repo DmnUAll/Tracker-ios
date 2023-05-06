3d907f175b35ed3843a396fdc6f0b48dc48bd35c
Ilya Valito <valito.spb@gmail.com>
2023-05-06 16:47:31 +0300

    ViewControllers dark\light mode screenshot-tests have been added

--- Tracker-iosTests/TrackerTests.swift
+++ Tracker-iosTests/TrackerTests.swift
@@ -19,8 +19,56 @@ final class TrackerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testViewController() throws {
+    func testLaunchScreen() throws {
+        let viewController = LaunchScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testOnboardingScreen() throws {
+        let viewController = OnboardingPageController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testTrackersScreen() throws {
         let viewController = TrackersScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testTrackerChoosingScreen() throws {
+        let viewController = TrackerChoosingScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testHabitCreationScreen() throws {
+        let viewController = HabitCreationScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testTrackerCategoryScreen() throws {
+        let viewController = TrackerCategoryScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testCategoryCreationScreen() throws {
+        let viewController = CategoryCreationScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testScheduleConfigurationScreen() throws {
+        let viewController = ScheduleConfigurationScreenController()
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
+        assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
+    }
+
+    func testStatisticsScreen() throws {
+        let viewController = StatisticsScreenController()
         assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .light))])
         assertSnapshots(matching: viewController, as: [.image(traits: .init(userInterfaceStyle: .dark))])
     }
