4bf98c9df7fad2d9c01def5b139e0b6489867a27
Ilya Valito <82824022+DmnUAll@users.noreply.github.com>
2023-06-03 17:57:20 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-Tracker-ios/Pods-Tracker-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tracker_ios : NSObject
-@end
-@implementation PodsDummy_Pods_Tracker_ios
-@end
--- Pods/Target Support Files/Pods-Tracker-iosTests/Pods-Tracker-iosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tracker_iosTests : NSObject
-@end
-@implementation PodsDummy_Pods_Tracker_iosTests
-@end
